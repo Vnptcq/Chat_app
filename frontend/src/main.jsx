2a25689e54465de6c82f5d681f4221650caf4f32
hndcmm245 <hoangan240505@gmail.com>
2025-03-05 20:28:31 +0700

    nhánh của em an

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './css/Login.css'; // Đảm bảo đường dẫn chính xác
+import Login from './pages/Login'; // Import component Login
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    
+    <Login />
+  </StrictMode>
+);
