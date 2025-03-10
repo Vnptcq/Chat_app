c499e84cb58e399bdfb65935b0ed936d4fb340ed
tienanh2401 <144700017+tienanh2401@users.noreply.github.com>
2025-03-10 21:09:08 +0700

    tien anh / 10-03-2025
    update profile

--- frontend/src/pages/ProfileCard.jsx
+++ frontend/src/pages/ProfileCard.jsx
@@ -5,22 +5,25 @@ const ProfileCard = () => {
   const [activeTab, setActiveTab] = useState("one");
 
   return (
-    <div className="col-sm-6 col-md-4 col-lg-3 mt-4">
-      <div className="card">
-        <img className="card-img-top" src="https://picsum.photos/200/150/?random" alt="Banner" />
-        <div className="card-block text-center">
-          <figure className="profile">
+    <div className="profile-card-container col-sm-6 col-md-4 col-lg-3 mt-4">
+      <div className="profile-card card">
+        {/* Ảnh Banner */}
+        <img className="profile-banner card-img-top" src="https://picsum.photos/200/150/?random" alt="Banner" />
+
+        {/* Thông tin người dùng */}
+        <div className="profile-card-body card-block text-center">
+          <figure className="profile-avatar-wrapper">
             <img src="https://picsum.photos/200/150/?random" className="profile-avatar rounded-circle" alt="Avatar" />
           </figure>
-          <h4 className="card-title mt-3">NameName</h4>
-          <div className="meta">
+          <h4 className="profile-name card-title mt-3">NameName</h4>
+          <div className="profile-status meta">
             <span className="badge badge-primary">Friends</span>
           </div>
-          <p className="card-text">hihi</p>
+          <p className="profile-description card-text">hihi</p>
         </div>
 
-        {/* Tabs */}
-        <div className="card-footer">
+        {/* Tabs điều hướng */}
+        <div className="profile-tabs card-footer">
           <ul className="nav nav-tabs card-header-tabs">
             {["one", "two", "three"].map((tab) => (
               <li className="nav-item" key={tab}>
@@ -35,33 +38,35 @@ const ProfileCard = () => {
           </ul>
         </div>
 
-        {/* Tab Content */}
-        <div className="tab-content p-3">
+        {/* Nội dung tab */}
+        <div className="profile-tab-content tab-content p-3">
           {activeTab === "one" && (
-            <div>
-              <h5 className="card-title">Tab Card One</h5>
-              <p className="card-text">Some quick example text to build on the card title.</p>
+            <div className="profile-tab-pane">
+              <h5 className="profile-tab-title card-title">Tab Card One</h5>
+              <p className="profile-tab-text card-text">Some quick example text to build on the card title.</p>
               <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
           )}
           {activeTab === "two" && (
-            <div>
-              <h5 className="card-title">Tab Card Two</h5>
-              <p className="card-text">Some quick example text to build on the card title.</p>
+            <div className="profile-tab-pane">
+              <h5 className="profile-tab-title card-title">Tab Card Two</h5>
+              <p className="profile-tab-text card-text">Some quick example text to build on the card title.</p>
               <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
           )}
           {activeTab === "three" && (
-            <div>
-              <h5 className="card-title">Tab Card Three</h5>
-              <p className="card-text">Some quick example text to build on the card title.</p>
+            <div className="profile-tab-pane">
+              <h5 className="profile-tab-title card-title">Tab Card Three</h5>
+              <p className="profile-tab-text card-text">Some quick example text to build on the card title.</p>
               <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
           )}
         </div>
       </div>
     </div>
+     
   );
+  
 };
 
 export default ProfileCard;
